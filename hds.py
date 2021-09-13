1c59252498f508eaed9873d823d62d28366ed42e
FabianStammen <f.stammen@yahoo.de>
2021-09-13 14:34:39 +0200

    updated for selenium 4, added a logfile , and optimized download section for new site behavior

--- hds.py
+++ hds.py
@@ -17,18 +17,21 @@ Example:
         $ python hds.py em
 
 It is required to have firefox installed and a version of geckodriver in the same folder as the
-program. (https://github.com/mozilla/geckodriver/releases\).
+program. (https://github.com/mozilla/geckodriver/releases).
 """
 import os
 import re
+import shutil
 import sys
 import time
 import zipfile
-from datetime import datetime
+from datetime import datetime, timezone
 
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 
@@ -42,13 +45,12 @@ def setup_driver(download_dir):
         selenium.webdriver.Firefox: The setup geckodriver.
     """
     options = Options()
-    options.headless = True
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference('browser.download.folderList', 2)
-    profile.set_preference('browser.download.manager.showWhenStarting', False)
-    profile.set_preference('browser.download.dir', download_dir)
-    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
-    return webdriver.Firefox(options=options, firefox_profile=profile)
+    # options.headless = True
+    options.set_preference(name='browser.download.folderList', value=2)
+    options.set_preference(name='browser.download.manager.showWhenStarting', value=False)
+    options.set_preference(name='browser.download.dir', value=download_dir)
+    options.set_preference(name='browser.helperApps.neverAsk.saveToDisk', value='application/octet-stream')
+    return webdriver.Firefox(options=options)
 
 
 def scrap(output_folder, full=False):
@@ -58,17 +60,17 @@ def scrap(output_folder, full=False):
         output_folder: Path to the download folder.
         full (bool): Full download or incremental; default false.
     """
-    base_url = 'https://www.histdata.com'
+    base_url = 'http://www.histdata.com'
     url = base_url + '/download-free-forex-data/?/metastock/1-minute-bar-quotes'
-    driver = setup_driver(output_folder)
     commodities = ['WTI/USD', 'BCO/USD']
     indexes = ['SPX/USD', 'JPX/JPY', 'NSX/USD', 'FRX/EUR', 'UDX/USD',
                'UKX/GBP', 'GRX/EUR', 'AUX/AUD', 'HKX/HKD', 'ETX/EUR']
-
     os.makedirs(output_folder, exist_ok=True)
-    last_file = ''
+    # last_file = ''
     to_be_removed = set()
     current_year = datetime.now().year
+    if not os.listdir(output_folder):
+        full = True
     for old_file in sorted(os.listdir(output_folder)):
         if full:
             os.remove(os.path.join(output_folder, old_file))
@@ -77,37 +79,58 @@ def scrap(output_folder, full=False):
             to_be_removed.add(old_file[:-5])
         elif len(old_file) == 35 and int(old_file[25:-6]) < current_year:
             to_be_removed.add(old_file)
-        elif last_file != '' and last_file[:22] != old_file[:22]:
-            to_be_removed.add(last_file)
-        last_file = old_file
+        # removed as histdata now uploads only full month data and not partially filled ones
+        # elif last_file != '' and last_file[:22] != old_file[:22]:
+        #     test1 = last_file[:22]
+        #     test2 = old_file[:22]
+        #     to_be_removed.add(last_file)
+        # last_file = old_file
     for part in to_be_removed:
         if os.path.exists(os.path.join(output_folder, part)):
             os.remove(os.path.join(output_folder, part))
 
-    forex_pair_urls = [tag.get('href') for tag in BeautifulSoup(requests.get(url).text, 'lxml')
-                       .find(class_='page-content').select('a[title]')
-                       if tag.get_text() not in commodities and tag.get_text() not in indexes]
-    for i, forex_pair_url in enumerate(forex_pair_urls):
-        date_urls = [tag.get('href') for tag in BeautifulSoup(requests.get(
-            base_url + forex_pair_url).text, 'lxml').find(class_='page-content').select('a[title]')]
-        for j, date_url in enumerate(date_urls):
-            link = BeautifulSoup(requests.get(base_url + date_url).text, 'lxml').find(id='a_file')
-            if ''.join(link.get_text().rsplit('_', 1)) not in os.listdir(output_folder):
-                driver.get(base_url + date_url)
-                driver.execute_script('jQuery("#file_down").submit();')
-            print(
-                '\r' + ' Download: ' + str(i + 1).zfill(len(str(len(forex_pair_urls)))) + '/' + str(
-                    len(forex_pair_urls)) + ' forex pairs - ' + str(j + 1).zfill(
-                        len(str(len(date_urls))))
-                + '/' + str(len(date_urls)) + ' single files', end='', flush=True)
-    print('\n')
+    forex_pair_links = BeautifulSoup(requests.get(url).text, 'lxml')
+    forex_pair_link_elements = forex_pair_links.find(class_='page-content').select('a[title]')
+    forex_pair_urls = [tag.get('href') for tag in forex_pair_link_elements if
+                       tag.get_text() not in commodities and tag.get_text() not in indexes]
+    last_periods = {}
+    driver = setup_driver(output_folder)
 
-    driver.get('about:downloads')
-    while True:
-        time.sleep(5)
-        if not driver.find_elements_by_css_selector('button.downloadButton.downloadIconCancel'):
-            break
-    driver.quit()
+    driver.implicitly_wait(1)
+    try:
+        for i, forex_pair_url in enumerate(forex_pair_urls):
+            date_links = BeautifulSoup(requests.get(base_url + forex_pair_url).text, 'lxml')
+            date_link_elements = date_links.find(class_='page-content').select('a[title]')
+            reg = re.compile('\\d+.*')
+            last_period = reg.findall(date_link_elements[0].get('title'))[0]
+            last_periods[forex_pair_url.rsplit('/', 1)[-1]] = last_period
+            date_urls = [tag.get('href') for tag in date_link_elements]
+            for j, date_url in enumerate(date_urls):
+                link = BeautifulSoup(requests.get(base_url + date_url).text, 'lxml').find(id='a_file')
+                if ''.join(link.get_text().rsplit('_', 1)) not in os.listdir(output_folder):
+                    driver.get(base_url + date_url)
+                    driver.execute_script('jQuery("#file_down").submit();')
+                print(
+                    '\r' + ' Download: ' + str(i + 1).zfill(len(str(len(forex_pair_urls)))) + '/' + str(
+                        len(forex_pair_urls)) + ' forex pairs - ' + str(j + 1).zfill(
+                        len(str(len(date_urls))))
+                    + '/' + str(len(date_urls)) + ' single files', end='', flush=True)
+        print('\n')
+
+        driver.get('about:downloads')
+        while True:
+            time.sleep(1)
+            try:
+                driver.find_element(By.CSS_SELECTOR, 'button.downloadButton.downloadIconCancel')
+            except NoSuchElementException:
+                break
+        with open(os.path.join(output_folder, 'output.log'), 'w') as log:
+            log.write('Historic Forex Data \nLast date checked: ' + datetime.now(timezone.utc).isoformat() +
+                      '\nTyp of check: ' + ('full' if full else 'incremental') + '\nLast periods of available data:')
+            for currency, period in last_periods.items():
+                log.write('\n\t' + currency + ': ' + period)
+    finally:
+        driver.quit()
 
 
 def extract(input_folder, output_folder, full=False):
@@ -133,6 +156,9 @@ def extract(input_folder, output_folder, full=False):
                     zip_ref.extractall(output_folder)
         print('\r' + ' Extracting: ' + str(i + 1).zfill(len(str(len(files)))) + '/' + str(
             len(files)), end='', flush=True)
+    else:
+        if file.endswith('.log'):
+            shutil.copyfile(os.path.join(input_folder, file), os.path.join(output_folder, file))
     print('\n')
 
 
@@ -160,6 +186,9 @@ def merge(input_folder, output_folder):
                     outfile.write(line)
         print('\r' + ' Merging: ' + str(i + 1).zfill(len(str(len(csv_files)))) + '/'
               + str(len(csv_files)), end='', flush=True)
+    if os.path.exists(os.path.join(input_folder, 'output.log')):
+        shutil.copyfile(os.path.join(input_folder, 'output.log'), os.path.join(output_folder, 'output.log'))
+
     print('\n')
 
 
@@ -173,7 +202,7 @@ def main():
     data_dir = os.path.join(root_dir, 'data')
     zip_dir = os.path.join(data_dir, 'zipped')
     raw_dir = os.path.join(data_dir, 'raw')
-    out_dir = os.path.join(data_dir, 'output')
+    out_dir = os.path.join(data_dir, 'Forex-1M')
 
     if len(sys.argv) == 1:
         scrap(zip_dir, False)
